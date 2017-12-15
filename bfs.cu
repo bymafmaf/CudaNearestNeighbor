26b1e11539843474869797d511b8b83121c33c8e
Muvaffak Onus <onus.muvaffak@gmail.com>
2017-12-15 18:45:18 +0300

    - inputLines is now 1D array with width.

--- bfs.cu
+++ bfs.cu
@@ -5,23 +5,26 @@
 
 #include <fstream>
 
-const int N = 512;
+//const int N = 512;
 using namespace::std;
 
-int inputLines[1000][16];
+const int BLOCKS = 1000;
+const int DIMENSIONS = 16;
+
+int inputLines[BLOCKS*DIMENSIONS];
 string filename = "test.txt";
 
-void parseLine(string line, int lineArr[]) {
+void parseLine(string line, int startIndex) {
   int start=0;
-  int numIndex = 0;
+  int numIndex = startIndex;
   for (size_t i = 0; i < line.length(); i++) {
     if (line[i] == ',') {
-      lineArr[numIndex] = stoi(line.substr(start, i-start));
+      inputLines[numIndex] = stoi(line.substr(start, i-start));
       numIndex++;
       start = i+1;
     }
   }
-  lineArr[numIndex] = stoi(line.substr(start, line.length()-start));
+  inputLines[numIndex] = stoi(line.substr(start, line.length()-start));
 }
 
 void readFile(){
@@ -30,20 +33,44 @@ void readFile(){
   int ind = 0;
   string line;
   while (getline(inputFile, line)) {
-    parseLine(line, inputLines[ind]);
+    parseLine(line, ind*DIMENSIONS);
     ind++;
   }
 }
 
-/*
-You can ignore the ewgths and vwghts. They are there as the read function expects those values
-row_ptr and col_ind are the CRS entities. nov is the Number of Vertices
-*/
+// __global__
+// calculateDifference(int *all, int *diffSquare, int id){
+//   int self = all[id][threadIdx.x];
+//   int other = all[blockIdx.x][threadIdx.x];
+//
+//   int result = other - self;
+//   diffSquare[threadIdx.x] += result * result;
+// }
 
 int main(int argc, char *argv[]) {
   readFile();
-
-
-
+  // int* d_inputLines;
+  // int* d_diffLines;
+  // int* diffLines;
+  // int lineSize = DIMENSIONS * sizeof(int);
+  // int allLinesSize = BLOCKS * lineSize;
+  //
+  // diffLines = (int *)malloc(allLinesSize);
+  // cudaMalloc((void **)&d_inputLines, allLinesSize);
+  // cudaMalloc((void **)&d_diffLines, allLinesSize);
+  //
+  // cudaMemcpy(d_inputLines, inputLines, allLinesSize, cudaMemcpyHostToDevice);
+  //
+  // calculateDifference<<<BLOCKS, DIMENSIONS>>>(d_inputLines, d_diffLines, 0);
+  //
+  // cudaMemcpy(diffLines, d_diffLines, allLinesSize, cudaMemcpyDeviceToHost);
+  //
+  // for (size_t i = 0; i < 16; i++) {
+  //   cout << diffLines[0][i] << ",";
+  // }
+  // cout << endl;
+  //
+  // free(diffLines);
+  // cudaFree(d_diffLines); cudaFree(d_inputLines);
   return 1;
 }
